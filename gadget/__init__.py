01ee45d61831458a2042465b1300128fe6b411e7
Douglas Hanley <thesecretaryofwar@gmail.com>
2024-08-16 10:35:36 -0500

    add embed to top level

--- gadget/__init__.py
+++ gadget/__init__.py
@@ -5,11 +5,13 @@ from . import loader
 from . import compute
 from . import model
 from . import bert
+from . import embed
 
 from .ggml import GGMLQuantizationType
 from .loader import GgufFile
 from .compute import GgmlCompute
 from .model import GgmlModel
 from .bert import BertModel
+from .embed import EmbedModel
 
 __version__ = "0.1"
