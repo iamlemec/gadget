bc1c53c42d633621c1bfd2a45cffc3593f64884a
Douglas Hanley <thesecretaryofwar@gmail.com>
2024-08-16 02:01:15 -0500

    strip out llama.cpp

--- gadget/__init__.py
+++ gadget/__init__.py
@@ -1,27 +1,14 @@
 # gadget
 
-from .llama import (
-    LLAMA_POOLING_TYPE_UNSPECIFIED,
-    LLAMA_POOLING_TYPE_NONE,
-    LLAMA_POOLING_TYPE_MEAN,
-    LLAMA_POOLING_TYPE_CLS,
-    LLAMA_POOLING_TYPE_LAST,
-)
-
-from .ggml import (
-    GGMLQuantizationType,
-    ggml_get_rows,
-    ggml_add,
-    ggml_add_inplace,
-    ggml_view_1d,
-)
-
+from . import loader
 from . import compute
 from . import model
+from . import bert
 
 from .loader import GgufFile
 from .compute import GgmlCompute
 from .model import GgmlModel
-from .embed import LlamaModel
+from .bert import BertModel
 
 __version__ = "0.1"
+
